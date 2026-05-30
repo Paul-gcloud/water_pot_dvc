5c3fbdd518fc31ea098392dba8055359ced967a7
Paul <faithvineco@gmail.com>
2026-05-30 21:18:24 +0300

    Added pipeline median n_estimators 1000

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -10,12 +10,12 @@ def load_data(filepath:str)->pd.DataFrame:
     except Exception as e:
         raise Exception(f'Error loading data from {filepath}:{e}')
 
-def fill_missing_values_with_mean(df):
+def fill_missing_values_with_median(df):
     try:
         for column in df.columns:
             if df[column].isnull().any():
-                mean_values=df[column].mean()
-                df[column]=df[column].fillna(mean_values)
+                median_values=df[column].median()
+                df[column]=df[column].fillna(median_values)
         return df
     except Exception as e:
         raise Exception(f'Error filling the missing : {e}')
@@ -36,11 +36,11 @@ def main():
         train_data = load_data(os.path.join(raw_data_path, 'train_data.csv'))
         test_data = load_data(os.path.join(raw_data_path, 'test_data.csv'))
 
-        processed_train = fill_missing_values_with_mean(train_data)
-        processed_test = fill_missing_values_with_mean(test_data)
+        processed_train = fill_missing_values_with_median(train_data)
+        processed_test = fill_missing_values_with_median(test_data)
 
-        save_data(processed_train, os.path.join(processed_data_path, 'train_processed_mean.csv'))
-        save_data(processed_test, os.path.join(processed_data_path, 'test_processed_mean.csv'))
+        save_data(processed_train, os.path.join(processed_data_path, 'train_processed_median.csv'))
+        save_data(processed_test, os.path.join(processed_data_path, 'test_processed_median.csv'))
     except Exception as e:
         raise Exception(f'Error occured :{e}')
 if __name__=="__main__":
--- src/model/model_building.py
+++ src/model/model_building.py
@@ -53,7 +53,7 @@ def save_model(model:RandomForestClassifier, filepath:str)->None:
 
 def main():
     try:
-        data_path = './data/processed/train_processed_mean.csv'
+        data_path = './data/processed/train_processed_median.csv'
         params_path='./params.yaml'
         model_name= 'models/model.pkl'
 
